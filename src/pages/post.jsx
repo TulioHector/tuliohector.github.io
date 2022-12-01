d188962ca29492488b0de2be35fa6cc05556d8d0
Hector Romano <tulio.hector@gmail.com>
2022-12-01 15:23:32 -0300

    add meta og tags

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -33,17 +33,17 @@ class Post extends Component {
     }
 
     async getTask() {
-        try{
+        try {
             const idPost = Number(this.idPost);
             const postUrl = `/posts/${this.postName}`;
             const postDb = Database.getPOstById(idPost);
             const response = await fetch(postUrl + '.md');
             const mdFile = await response.text();
-            
+
             this.setState({ postContent: mdFile });
-            
-            const result = await postDb.then((item) => {     
-                let post;           
+
+            const result = await postDb.then((item) => {
+                let post;
                 item.forEach((doc) => {
                     // doc.data() is never undefined for query doc snapshots
                     //console.log(doc.id, " => ", doc.data());
@@ -51,62 +51,75 @@ class Post extends Component {
                 });
                 return post;
             }).catch(err => console.log(err));
-            
+
             let pageConfig = this.context.pageSettings;
             const settings = {
                 ...pageConfig,
-                backgroundImage : `url('../../assets/posts/${idPost}/${result.cover}')`,
-                pageTitle : "",
-                pageSubTitle : "",
+                backgroundImage: `url('../../assets/posts/${idPost}/${result.cover}')`,
+                pageTitle: "",
+                pageSubTitle: "",
             }
             this.context.setPageSettings(settings);
-            return result;            
-        }catch(error) {
+            this.setState({
+                postInfo: {...result}
+            });
+            return result;
+        } catch (error) {
             console.log(error);
         }
     }
 
     async componentDidMount() {
-        try{
+        try {
             await this.getTask();
-        }catch(error) {
+        } catch (error) {
             console.log(error);
-        }        
+        }
     }
 
     render() {
-        return (
-            <>
-            <PageHeader />
-            <Helmet>
-                <title>{this.postName}</title>
-                <link rel="canonical" href={`https://tuliohector.github.io/${this.postName}`} />
-            </Helmet>
-            <article className="mb-4">
-                <div className="container px-4 px-lg-5">
-                    <div className="row gx-4 gx-lg-5 justify-content-center">
-                        <div className="col-md-10 col-lg-8 col-xl-7">
-                            <Markdown
-                                options={{
-                                    overrides: {
-                                        Code: {
-                                            component: Code,
-                                            props: {
-                                                isDark: this.state.isDark,
-                                                setIsDark: this.state.setIsDark
+        if(this.state.postInfo !== undefined) {
+            return (
+                <>
+                    <PageHeader />
+                    <Helmet>
+                        {console.log(this.state.postInfo)}
+                        <title>{this.postName}</title>
+                        <link rel="canonical" href={`https://tuliohector.github.io/posts/${this.postName}/${this.idPost}`} data-react-helmet="true"/>
+                        <meta property="og:title" content={`${this.state.postInfo.Title}`} data-react-helmet="true"/>
+                        <meta property="og:type" content="website" data-react-helmet="true"/>
+                        <meta property="og:url" content={`https://tuliohector.github.io/posts/${this.postName}/${this.idPost}`} data-react-helmet="true"/>
+                        <meta property="og:image" content={`https://tuliohector.github.io/assets/posts/${this.idPost}/${this.state.postInfo.cover}`} data-react-helmet="true"/>
+                        <meta property='og:description' content={`${this.state.postInfo.description}`}/>
+                    </Helmet>
+                    <article className="mb-4">
+                        <div className="container px-4 px-lg-5">
+                            <div className="row gx-4 gx-lg-5 justify-content-center">
+                                <div className="col-md-10 col-lg-8 col-xl-7">
+                                    <Markdown
+                                        options={{
+                                            overrides: {
+                                                Code: {
+                                                    component: Code,
+                                                    props: {
+                                                        isDark: this.state.isDark,
+                                                        setIsDark: this.state.setIsDark
+                                                    }
+                                                }
                                             }
-                                        }
-                                    }
-                                }}
-                            >
-                                {this.state.postContent}
-                            </Markdown>
+                                        }}
+                                    >
+                                        {this.state.postContent}
+                                    </Markdown>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </article>
-            </>
-        );
+                    </article>
+                </>
+            );
+        }else {
+            return null;
+        }        
     }
 }
 
